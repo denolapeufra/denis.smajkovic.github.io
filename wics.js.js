5f1795e7b31a4c0884698aef8c4e41938ea7bb7c
denolapeufra <157356862+denolapeufra@users.noreply.github.com>
2024-03-11 12:11:24 +0100

    Add files via upload

---
+++ wics.js.js
@@ -0,0 +1,30 @@
+alert("WiCs");
+
+//var testo = "ciaone";
+//var buleano = true;
+
+let numero = 123;
+let virgola = 12;
+    
+//alert(typeof(buleano));
+
+let campo = document.getElementById("search2");
+alert(typeof(campo));
+
+campo.style.background = "red";
+campo.value = "Nikimmok wled l97ab";
+
+let title = document.getElementsByClassName("learncodeh1");
+//alert(title, length);
+let contenuto = title.innerHTML;
+alert(contenuto);
+title[0].innerHTML = "Code to learn";*/
+
+var testo = "ciaone";
+alert(testo.lenght);
+for(let i=0;<testo.lenght;i++){
+    alert(testo[i]);
+    console.log(testo[i]);
+ }
+    
+}
